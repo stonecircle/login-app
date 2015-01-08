90099a2c1dd1aeaa651d6b7adc4f603ebe0a9918
ynnoj <jonathan.steele@me.com>
2015-01-08 18:57:47 +0000

    Localised strings for all templates

---
+++ app/initializers/i18n.js
@@ -0,0 +1,80 @@
+import Ember from 'ember';
+
+var translations = {
+  // Button titles
+  button: {
+    'signIn': "Sign in",
+    'signUp': "Sign up",
+    'reset': "Reset password",
+    'request': "Request confirmation email",
+    'verify': "Verify account",
+    'download': "Download"
+  },
+  // Form labels
+  label: {
+    'email': "Email",
+    'password': "Password",
+    confirm: {
+      'email': "Confirm Email",
+      'password': "Confirm Password"
+    }
+  },
+  // Sign in page
+  signIn: {
+    label: {
+      'forgot': "Forgot your password"
+    }
+  },
+  // Sign up page
+  signUp: {
+    label: {
+      'terms': "I have read and accept the"
+    }
+  },
+  //Download page
+  download: {
+    error: {
+      'title': "Fiddlesticks",
+      message: {
+        'one': "Need help getting it sorted",
+        'two': "Drop us a line"
+      }
+    },
+    success: {
+      'title': "Download",
+      message: {
+        'one': "Thanks! Your account and payment have been verified. Click below to download",
+        'two': "and start the installation process"
+      }
+    }
+  },
+  // Account enabled page
+  accountEnabled: {
+    'title': "Your account is now enabled!",
+    'message': "You can now sign in with the e-mail and password you used when registering"
+  },
+  // Confirm password page
+  confirmPassword: {
+    error: {
+      'title': "There has been an error"
+    },
+    change: {
+      'title': "Welcome back",
+      'message': "All we need is your new password to keep everything nice and secure"
+    },
+    success: {
+      'title': "Password updated successfully",
+      'message': "You can now use your new password to login"
+    }
+  }
+};
+
+var i18n = {
+  name: 'i18n',
+  initialize: function() {
+    Ember.I18n.translations = translations;
+    Ember.FEATURES.I18N_TRANSLATE_HELPER_SPAN = false;
+  }
+};
+
+export default i18n;
--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    urlPrefix: 'local',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -22,16 +23,16 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
     // Testem prefers this...
     ENV.baseURL = '/';
-    ENV.locationType = 'auto';
+    ENV.locationType = 'none';
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
@@ -41,7 +42,11 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.urlPrefix =  '';
+  }
 
+  if (environment === 'staging') {
+    ENV.urlPrefix =  'staging';
   }
 
   return ENV;
