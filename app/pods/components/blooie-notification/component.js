6fc72ec9da80c6af138c8e04395507367ce10694
ynnoj <jonathan.steele@me.com>
2015-01-14 12:14:00 +0000

    Notification icon is now defined by the notification type

--- app/pods/components/blooie-notification/component.js
+++ app/pods/components/blooie-notification/component.js
@@ -3,6 +3,20 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     classNameBindings: [':notification', 'notification.type', 'dismiss::in', ':pad1', ':center-block'],
 
+    // Set icon depending on notification type
+    notificationIcon: function() {
+        switch(this.get('notification.type')){
+            case "info":
+                return 'fa-info-circle';
+            case "success":
+                return 'fa-check';
+            case "warning":
+                return 'fa-warning';
+            case "error":
+                return 'fa-warning';
+        }
+    }.property('notification.type'),
+
     actions: {
         removeNotification: function() {
             this.set('dismiss', true);
