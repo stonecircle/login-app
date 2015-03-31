f10642c7b563b947070c271d44d1e89fe9b578c4
Chris Manson <chris@manson.ie>
2015-03-31 19:34:12 +0100

    running with no errors

--- config/environment.js
+++ config/environment.js
@@ -18,6 +18,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'style-src': "'self' 'unsafe-inline'"
     }
   };
 
