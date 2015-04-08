836cb2e5bdda94d92233931c87b9641105a9a0a6
Darragh O'Keeffe <darragh@bloo.ie>
2015-04-08 11:14:02 +0100

    remove extra comma

--- app/pods/authorize/route.js
+++ app/pods/authorize/route.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     redirect: function(model, transition) {
         this.transitionTo('signin', {queryParams: transition.queryParams});
-    },
+    }
 });
