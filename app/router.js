74f46401448be6b37da73f6e0a54ad4756c5c523
thedoktor <hey@darraghokeeffe.com>
2015-04-09 14:41:52 +0100

    render sign in page without signup for admin route

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('signin');
+  this.route('admin');
   this.route('authorize');
   this.route('signup');
   this.route('download');
