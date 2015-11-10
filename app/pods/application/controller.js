457edebf83611add8e108cbf3a90fc8e8d91dfad
thedoktor <hey@darraghokeeffe.com>
2015-11-10 13:39:32 +0000

    added previous_location param to sign up flow

--- app/pods/application/controller.js
+++ app/pods/application/controller.js
@@ -4,12 +4,13 @@ import config from 'authmaker-login-app/config/environment';
 export default Ember.Controller.extend({
   urlPrefix: config.urlPrefix,
 
-  queryParams: ['response_type', 'display', 'client_id', 'redirect_uri', 'scope', 'state'],
+  queryParams: ['response_type', 'display', 'client_id', 'redirect_uri', 'scope', 'state', 'previous_location'],
 
   response_type: '',
   display: '',
   client_id: '',
   redirect_uri: '',
   scope: '',
-  state: ''
+  state: '',
+  previous_location: ''
 });
