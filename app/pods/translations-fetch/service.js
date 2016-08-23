e91436b700db53d3b6c4ac2c1969d085980f8221
Jonathan Steele <jonathan@steele.pro>
2016-08-23 10:52:41 +0100

    Add return for JSON fetch

--- app/pods/translations-fetch/service.js
+++ app/pods/translations-fetch/service.js
@@ -6,7 +6,7 @@ export default Service.extend({
     i18n: inject.service(),
 
     fetch() {
-        Ember.$.getJSON('/i18n').then(this._addTranslations.bind(this));
+        return Ember.$.getJSON('/i18n').then(this._addTranslations.bind(this));
     },
 
     _addTranslations(json) {
