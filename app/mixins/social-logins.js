145a75c06b3a3cdcba570fe936e28b4594624ad1
Chris Manson <chris@manson.ie>
2018-12-09 20:12:54 +0000

    fixing social login implementation

--- app/mixins/social-logins.js
+++ app/mixins/social-logins.js
@@ -1,16 +1,24 @@
 import Mixin from '@ember/object/mixin';
 import { inject as controller } from '@ember/controller';
-import { computed } from '@ember/object';
+import { computed, get } from '@ember/object';
+import { alias } from '@ember/object/computed';
 
 export default Mixin.create({
   application: controller(),
 
   queryParams: ['logins'],
 
-  socialLogins: computed.alias('application.model.socialLogins'),
+  socialLogins: alias('application.model.socialLogins'),
+  onlySocialLogins: alias('application.model.onlySocialLogins'),
+  limitSocialLogins: alias('application.model.limitSocialLogins'),
 
   shownSocialLogins: computed('socialLogins.[]', 'logins.[]', function() {
     const socialLogins = this.get('socialLogins') || [];
+
+    if(!get(this, 'limitSocialLogins')) {
+      return socialLogins;
+    }
+
     return socialLogins.filter((login) => {
       return (this.get('logins') ? this.get('logins').split(',') : []).find(function(allow) {
         return login === allow;
